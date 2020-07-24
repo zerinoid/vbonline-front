a2bf46267e905e3ce50f207de8983bd5632ba022
zerinoid <leo.zerino@gmail.com>
2020-07-24 00:43:16 -0300

    bootstrap importado no scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
