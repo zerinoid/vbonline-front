6a79e6bf1ef198124bfdb10ed851f490f2cbd6ca
zerinoid <leo.zerino@gmail.com>
2020-07-31 20:19:56 -0300

    nav estruturada conforma projeto

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import ListaDocs from './components/lista-docs.component';
 
 import Logo from './assets/img/logo.png';
 
+const flexN = 4;
+
 function App() {
     return (
         <div className="App">
@@ -22,18 +24,57 @@ function App() {
                             flex-wrap: wrap;
                             align-items: end;
                             justify-content: space-between;
-                            padding: 0.5rem 1rem;
+                            padding: 0.5rem 0;
+                            min-height: 90px;
+                            div {
+                                display: flex;
+                            }
+                            & > div {
+                                margin-top: 35px;
+                            }
                         `}
                     >
-                        <img alt="Logo" src={Logo} />
-                        <Link to="/">
-                            <button>Docs</button>
-                        </Link>
-                        <Link to="/sobre">
-                            <button>VB Online</button>
-                        </Link>
-                        <button>pt</button>
-                        <button>en</button>
+                        <div
+                            css={css({
+                                flex: `1 ${flexN} 0`,
+                            })}
+                        >
+                            <Link to="/">
+                                <img
+                                    alt="Logo"
+                                    src={Logo}
+                                    css={css({
+                                        position: 'absolute',
+                                        left: -59,
+                                        top: 62,
+                                        'z-index': 99,
+                                    })}
+                                />
+                            </Link>
+                        </div>
+                        <div
+                            css={css({
+                                flex: `${flexN} 1 0`,
+                            })}
+                        >
+                            <Link to="/">
+                                <button>Docs</button>
+                            </Link>
+                        </div>
+                        <div
+                            css={css({
+                                flex: `1 ${flexN} 0`,
+                                justifyContent: 'space-between',
+                            })}
+                        >
+                            <Link to="/sobre">
+                                <button>VB Online</button>
+                            </Link>
+                            <div>
+                                <button>pt</button>
+                                <button>en</button>
+                            </div>
+                        </div>
                     </nav>
                     <Route path="/" exact component={ListaDocs} />
                     <Route path="/sobre" component={Sobre} />
