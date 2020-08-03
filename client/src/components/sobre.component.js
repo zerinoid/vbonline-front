b5113ded9d0eb7f2ccc76be061f33fed1d8c50c9
zerinoid <leo.zerino@gmail.com>
2020-08-03 19:03:39 -0300

    campos no back 'sobre'

--- client/src/components/sobre.component.js
+++ client/src/components/sobre.component.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
-import axios from "axios";
-
+import axios from 'axios';
 
 export default class Sobre extends Component {
-
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {};
     }
 
     componentDidMount() {
         axios.get('/api/sobre').then((res) => {
-            this.setState(res.data)
-        })
+            this.setState(res.data);
+        });
     }
 
     render() {
-        return (
-            <div className="sobre">{this.state.pt}</div>
-        );
+        return <div className="sobre">{this.state.pt.titulo}</div>;
     }
 }
