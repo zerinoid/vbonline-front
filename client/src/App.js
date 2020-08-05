96f09744002432300ec748eae77bb5357caccc5e
zerinoid <leo.zerino@gmail.com>
2020-08-05 15:45:32 -0300

    App convertido para class comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-// import React from 'react';
+import { Component } from 'react';
 import { css, jsx } from '@emotion/core';
 import './App.scss';
 
@@ -12,62 +12,62 @@ import Logo from './assets/img/logo2x.png';
 
 const flexN = 5;
 
-function App() {
-    return (
-        <div className="App">
-            <Router>
-                <nav>
-                    <div
-                        css={css({
-                            flex: `1 ${flexN} 0`,
-                        })}
-                    >
-                        <Link to="/">
-                            <img
-                                alt="Logo"
-                                src={Logo}
-                                css={css({
-                                    position: 'absolute',
-                                    left: -38,
-                                    top: 43,
-                                    'z-index': 99,
-                                    width: 200,
-                                })}
-                            />
-                        </Link>
-                    </div>
-                    <div
-                        css={css({
-                            flex: `${flexN} 1 0`,
-                        })}
-                    >
-                        <Link to="/">
-                            <button>Docs</button>
-                        </Link>
-                    </div>
-                    <div
-                        css={css({
-                            flex: `1 ${flexN} 30px`,
-                            justifyContent: 'space-between',
-                        })}
-                    >
-                        <Link to="/sobre">
-                            <button>VB Online</button>
-                        </Link>
-                        <div>
-                            <button>pt</button>
-                            <button>en</button>
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <nav>
+                        <div
+                            css={css({
+                                flex: `1 ${flexN} 0`,
+                            })}
+                        >
+                            <Link to="/">
+                                <img
+                                    alt="Logo"
+                                    src={Logo}
+                                    css={css({
+                                        position: 'absolute',
+                                        left: -38,
+                                        top: 43,
+                                        'z-index': 99,
+                                        width: 200,
+                                    })}
+                                />
+                            </Link>
                         </div>
+                        <div
+                            css={css({
+                                flex: `${flexN} 1 0`,
+                            })}
+                        >
+                            <Link to="/">
+                                <button>Docs</button>
+                            </Link>
+                        </div>
+                        <div
+                            css={css({
+                                flex: `1 ${flexN} 30px`,
+                                justifyContent: 'space-between',
+                            })}
+                        >
+                            <Link to="/sobre">
+                                <button>VB Online</button>
+                            </Link>
+                            <div>
+                                <button>pt</button>
+                                <button>en</button>
+                            </div>
+                        </div>
+                    </nav>
+
+                    <div className="conteudo">
+                        <Route path="/" exact component={ListaDocs} />
+                        <Route path="/sobre" component={Sobre} />
                     </div>
-                </nav>
-
-                <div className="conteudo">
-                    <Route path="/" exact component={ListaDocs} />
-                    <Route path="/sobre" component={Sobre} />
-                </div>
-            </Router>
-        </div>
-    );
+                </Router>
+            </div>
+        );
+    }
 }
-
-export default App;
