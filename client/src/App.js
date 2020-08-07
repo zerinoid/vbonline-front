bc2343a394ec3c4ff0a17b5ded3a71fa4b096064
zerinoid <leo.zerino@gmail.com>
2020-08-07 02:36:06 -0300

    conteudo de App() condicionado ao state "showPlayer"

--- client/src/App.js
+++ client/src/App.js
@@ -54,71 +54,74 @@ export default class App extends Component {
     render() {
         return (
             <div className="App">
-                <Router>
-                    <nav>
-                        <div
-                            css={css({
-                                flex: `1 ${flexN} 0`,
-                            })}
-                        >
-                            <Link to="/">
-                                <img
-                                    alt="Logo"
-                                    src={Logo}
-                                    css={css({
-                                        position: 'absolute',
-                                        left: -38,
-                                        top: 43,
-                                        'z-index': 99,
-                                        width: 200,
-                                    })}
-                                />
-                            </Link>
-                        </div>
-                        <div
-                            css={css({
-                                flex: `${flexN} 1 0`,
-                            })}
-                        >
-                            <Link to="/">
-                                <button>Docs</button>
-                            </Link>
-                        </div>
-                        <div
-                            css={css({
-                                flex: `1 ${flexN} 30px`,
-                                justifyContent: 'space-between',
-                            })}
-                        >
-                            <Link to="/sobre">
-                                <button>VB Online</button>
-                            </Link>
-                            <div>
-                                <button>pt</button>
-                                <button>en</button>
-                            </div>
-                        </div>
-                    </nav>
-                    <div className="conteudo">
-                        {this.state.data ? (
-                            <Route
-                                path="/"
-                                exact
-                                render={(props) => (
-                                    <ListaDocs
-                                        {...props}
-                                        lista={this.state}
-                                        playVideo={this.togglePlayerHandler}
+                {this.state.showPlayer ? (
+                    <VideoPlayer {...this.videoJsOptions} />
+                ) : (
+                    <Router>
+                        <nav>
+                            <div
+                                css={css({
+                                    flex: `1 ${flexN} 0`,
+                                })}
+                            >
+                                <Link to="/">
+                                    <img
+                                        alt="Logo"
+                                        src={Logo}
+                                        css={css({
+                                            position: 'absolute',
+                                            left: -38,
+                                            top: 43,
+                                            'z-index': 99,
+                                            width: 200,
+                                        })}
                                     />
-                                )}
-                            />
-                        ) : (
-                            <div>ERRO</div>
-                        )}
-                        <Route path="/sobre" component={Sobre} />
-                    </div>
-                </Router>
-                {this.state.showPlayer ? this.togglePlayerHandler() : null}
+                                </Link>
+                            </div>
+                            <div
+                                css={css({
+                                    flex: `${flexN} 1 0`,
+                                })}
+                            >
+                                <Link to="/">
+                                    <button>Docs</button>
+                                </Link>
+                            </div>
+                            <div
+                                css={css({
+                                    flex: `1 ${flexN} 30px`,
+                                    justifyContent: 'space-between',
+                                })}
+                            >
+                                <Link to="/sobre">
+                                    <button>VB Online</button>
+                                </Link>
+                                <div>
+                                    <button>pt</button>
+                                    <button>en</button>
+                                </div>
+                            </div>
+                        </nav>
+                        <div className="conteudo">
+                            {this.state.data ? (
+                                <Route
+                                    path="/"
+                                    exact
+                                    render={(props) => (
+                                        <ListaDocs
+                                            {...props}
+                                            lista={this.state}
+                                            playVideo={this.togglePlayerHandler}
+                                        />
+                                    )}
+                                />
+                            ) : (
+                                <div>ERRO</div>
+                            )}
+                            <Route path="/sobre" component={Sobre} />
+                        </div>
+                    </Router>
+                )}
             </div>
         );
     }
