d17bc5d236c19364944a2d10b7c9252100d9633b
Eduardo Haddad <eduardo.torquemada@gmail.com>
2020-08-13 22:11:27 -0300

    Session para idiomas

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 // Controllers
+const langController = require('../controllers/lang');
 const sobreController = require('../controllers/sobre');
 const listaDocsController = require('../controllers/lista-docs');
 const saibaMaisController = require('../controllers/saiba-mais');
+
+// GET /api/lang/set/:name
+router.get('/lang/set/:name', langController.setLang);
+
+// GET /api/lang/get/:name
+router.get('/lang/get', langController.getLang);
 
 // GET /api/sobre
 router.get('/sobre', sobreController.getSobre);
