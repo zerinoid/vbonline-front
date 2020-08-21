eb2c63a4aa5349adba0133d8c223350a0075b044
zerinoid <leo.zerino@gmail.com>
2020-08-20 21:41:22 -0300

    mainPreview com aspect ratio

--- client/src/components/lista-docs.component.js
+++ client/src/components/lista-docs.component.js
@@ -12,118 +12,113 @@ import smallPlay from '../assets/img/sm_play.png';
 import hvSmallPlay from '../assets/img/hv_sm_play.png';
 import hvBigPlay from '../assets/img/hv_big_play.png';
 
-const mainHeight = 620;
-const thumbHeight = 140;
-const previewMargin = 6;
-
-const DocPreviewContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    @media (max-width: 992px) {
-        flex-direction: column;
-        > div {
-            margin-bottom: ${previewMargin}px;
-        }
-        > div:last-child {
-            margin-bottom: 0px;
-        }
-        h4 {
-            margin-bottom: 0;
-        }
-    }
-`;
-
-const DocPreviewMain = styled.div`
-    width: 100%;
-    height: ${mainHeight}px;
-    padding: 10% 10%;
-    margin-bottom: ${previewMargin}px;
-    background: url(${(props) => props.bg}) center/cover no-repeat;
-    color: white;
-    position: relative;
-    h3 {
-        text-transform: none;
-    }
-    button {
-        border: none;
-        position: relative;
-    }
-    @media (max-width: 992px) {
-        height: calc(100vh - 90px);
-    }
-`;
-
-const DocPreviewThumb = (props) => (
-    <DocPreviewMain
-        css={css`
-            width: 32.95%;
-            height: ${thumbHeight}px;
-            padding: 12px;
-            display: inline-block;
-            margin: 0 0 0 0;
-            p {
-                font-family: FedraMono;
-                font-weight: normal;
-            }
-            &:hover {
-                box-shadow: inset 0px 0px 0px 2px #fff;
-            }
-            @media (max-width: 992px) {
-                width: 100%;
-                height: ${thumbHeight}px;
-            }
-        `}
-        {...props}
-    />
-);
-
-const Absolute = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-`;
-
-const PlayButton = (props) => {
-    return (
-        <Link
-            css={css`
-                margin: auto;
-            `}
-            to="/video"
-        >
-            <div
-                css={css({
-                    cursor: 'pointer',
-                    background: `url(${props.imagem}) center/${props.size}px no-repeat`,
-                    width: props.size,
-                    height: props.size,
-                    '&:hover': {
-                        background: `url(${props.hvimagem}) center/${props.hvsize}px no-repeat`,
-                        width: props.hvsize,
-                        height: props.hvsize,
-                    },
-                    '@media (max-width: 992px)': {
-                        '&:hover': {
-                            background: `url(${props.imagem}) center/${props.size}px no-repeat`,
-                            width: props.size,
-                            height: props.size,
-                        },
-                    },
-                })}
-                onClick={props.click}
-            />
-        </Link>
-    );
-};
-
 export default function ListaDocs(props) {
     const video_list = props.lista.data.videos;
     const lang = props.lang ? props.lang : 'pt';
-    const videoCount = video_list.lenght;
+
+    const DocPreviewContainer = styled.div`
+        display: flex;
+        justify-content: space-between;
+        @media (max-width: 992px) {
+            flex-direction: column;
+            > div {
+                margin-bottom: ${props.previewMargin}px;
+            }
+            > div:last-child {
+                margin-bottom: 0px;
+            }
+            h4 {
+                margin-bottom: 0;
+            }
+        }
+    `;
+
+    const DocPreviewMain = styled.div`
+        width: 100%;
+        padding-bottom: 38.1%;
+        margin-bottom: ${props.previewMargin}px;
+        background: url(${(props) => props.bg}) center/cover no-repeat;
+        color: white;
+        position: relative;
+        h3 {
+            text-transform: none;
+        }
+        button {
+            border: none;
+            position: relative;
+        }
+        @media (max-width: 992px) {
+            height: calc(100vh - 74px);
+            padding-bottom: unset;
+        }
+    `;
+
+    const DocPreviewThumb = (localProps) => (
+        <DocPreviewMain
+            css={css`
+                width: 32.95%;
+                height: ${props.thumbHeight}px;
+                padding: 12px;
+                display: inline-block;
+                margin: 0 0 0 0;
+                p {
+                    font-family: FedraMono;
+                    font-weight: normal;
+                }
+                &:hover {
+                    box-shadow: inset 0px 0px 0px 2px #fff;
+                }
+                @media (max-width: 992px) {
+                    width: 100%;
+                    height: ${props.thumbHeight}px;
+                }
+            `}
+            {...localProps}
+        />
+    );
+
+    const Absolute = styled.div`
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+    `;
+
+    const PlayButton = (props) => {
+        return (
+            <Link
+                css={css`
+                    margin: auto;
+                `}
+                to="/video"
+            >
+                <div
+                    css={css({
+                        cursor: 'pointer',
+                        background: `url(${props.imagem}) center/${props.size}px no-repeat`,
+                        width: props.size,
+                        height: props.size,
+                        '&:hover': {
+                            background: `url(${props.hvimagem}) center/${props.hvsize}px no-repeat`,
+                            width: props.hvsize,
+                            height: props.hvsize,
+                        },
+                        '@media (max-width: 992px)': {
+                            '&:hover': {
+                                background: `url(${props.imagem}) center/${props.size}px no-repeat`,
+                                width: props.size,
+                                height: props.size,
+                            },
+                        },
+                    })}
+                    onClick={props.click}
+                />
+            </Link>
+        );
+    };
 
     function Lista() {
         if (video_list && video_list.length > 0) {
