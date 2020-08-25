250bdd72ea04eafae5d68714fb377454393bb9c0
Eduardo Haddad <eduardo.torquemada@gmail.com>
2020-08-25 12:10:27 -0300

    Avanços no player

--- client/src/components/video.component.js
+++ client/src/components/video.component.js
@@ -3,10 +3,13 @@ import { useEffect, useRef, useState } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import { css, jsx } from '@emotion/core';
 import Player from '@vimeo/player';
+import 'flexboxgrid';
 
 import closeButton from '../assets/img/v_assets/vFechar.png'
 import fullScreenButton from '../assets/img/v_assets/vFullscreen.png'
 import infoButton from '../assets/img/v_assets/vMais.png'
+import playlistButton from '../assets/img/v_assets/vFila.png'
+import nextButton from '../assets/img/v_assets/vNext.png'
 
 const VideoPlayer = (props) => {
     const history = useHistory();
@@ -17,6 +20,7 @@ const VideoPlayer = (props) => {
 
     // Infobox state
     const [infoBoxState, setInfoBoxState] = useState("none");
+    const [playlistBoxState, setPlaylistBoxState] = useState("none");
 
     // Methods
     const closePlayer = () => {
@@ -37,17 +41,32 @@ const VideoPlayer = (props) => {
         } else {
             setInfoBoxState("none");
         }
-        
     };
+    
+    const togglePlaylistBox = () => {
+        if(playlistBoxState == "none"){
+            setPlaylistBoxState("block");
+        } else {
+            setPlaylistBoxState("none");
+        }
+    };
+
+    const goToNextVideo = () => {};
 
     // didMount
     useEffect(() => {
-        // instantiate vimeo player
+        // Instantiate vimeo player
         player = new Player('vimeo-player', props);
 
-        // Fullscreen on player load
+        // Player loaded
         player.on('loaded', () => {
 
+            // Minimum volume
+            player.getVolume().then((vol) => {
+                if(vol < 0.5){
+                    player.setVolume(0.5);
+                }
+            });
 
             // player
             //     .requestFullscreen()
@@ -84,17 +103,55 @@ const VideoPlayer = (props) => {
     });
 
     return (
+
         <div id="video-outter">
             <div id="video-container">
                 <div id="vimeo-player" ref={playerRef} />
             </div>
             <div id="video-info">
-                <img src={closeButton} onClick={closePlayer} className="bt-player" />
-                <img src={fullScreenButton} onClick={enterFullScreen} className="bt-player" />
-                <img src={infoButton} onClick={toggleInfoBox} className="bt-player" />
-                <div id="info-box" style={{display: infoBoxState}}>
-                    aaaaa
-                </div>
+                <div className="row">
+                    <div className="col-xs-6"></div>
+                    <div className="col-xs-6">
+                        <div className="row video-buttons">
+                            <div className="col-xs-2 col-xs-offset-1">
+                                <img src={infoButton} onClick={toggleInfoBox} className="bt-player" />
+                            </div>
+                            <div className="col-xs-2">
+                                <img src={playlistButton} onClick={togglePlaylistBox} className="bt-player" />
+                            </div>
+                            <div className="col-xs-2">
+                                <img src={nextButton} onClick={goToNextVideo} className="bt-player" />
+                            </div>
+                            <div className="col-xs-2">
+                                <img src={fullScreenButton} onClick={enterFullScreen} className="bt-player" />
+                            </div>
+                            <div className="col-xs-2">
+                                <img src={closeButton} onClick={closePlayer} className="bt-player" />
+                            </div>
+                        </div>
+                        <div className="row video-boxes">
+                            <div className="info-box col-xs-2 col-xs-offset-1">
+                                <div style={{display: infoBoxState}}>
+                                    aaa
+                                </div>
+                            </div>
+                            <div className="info-box col-xs-2">
+                                <div style={{display: playlistBoxState}}>
+                                    bbb
+                                </div>
+                            </div>
+                            <div className="col-xs-2">
+                                3
+                            </div>
+                            <div className="col-xs-2">
+                                F
+                            </div>
+                            <div className="col-xs-2">
+                                C
+                            </div>
+                        </div>
+                    </div>
+                </div>        
             </div>
         </div>
     );
