dca708bf3c81fd0fe46ef7ff15ebc09df12fa3e8
zerinoid <leo.zerino@gmail.com>
2020-08-25 17:31:21 -0300

    ajuste margens sobre mobile

--- client/src/components/about-section.component.js
+++ client/src/components/about-section.component.js
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 
 const AboutSection = styled.div`
     margin-top: 6%;
+    @media (max-width: 992px) {
+        margin-top: 50px;
+    }
 `;
 
 export default AboutSection;
