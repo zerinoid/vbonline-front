7899bc0de40f962b3fa569ef06c9b38de11e340c
zerinoid <leo.zerino@gmail.com>
2020-08-28 14:09:22 -0300

    MOB: altura total mainPrev contabiliza overlay ios e android

--- client/src/components/lista-docs.component.js
+++ client/src/components/lista-docs.component.js
@@ -47,7 +47,7 @@ export default function ListaDocs(props) {
         }
         @media (max-width: 992px) {
             margin-bottom: ${previewMarginMobile};
-            height: calc(100vh - 72px);
+            height: calc(100vh - 128px);
             padding-bottom: unset;
         }
     `;
