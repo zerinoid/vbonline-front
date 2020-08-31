abd37eb18f0d3f37e40fc3bd099f00d66b60b01b
zerinoid <leo.zerino@gmail.com>
2020-08-31 19:36:31 -0300

    unset no hover menumobile

--- client/src/components/botao-menu-mobile.component.js
+++ client/src/components/botao-menu-mobile.component.js
@@ -4,16 +4,27 @@ import colors from '../styles/colors';
 
 import Botao from './botao.component';
 
-const BotaoMenuMobile = (props) => (
-    <Botao
-        css={{
-            backgroundColor: props.active ? colors.branco : colors.vermelho,
-            color: props.active ? colors.vermelho : colors.branco,
-            border: props.active ? 'none' : `solid 1px ${colors.branco}`,
-            fontWeight: props.active ? 'bold' : 'normal',
-        }}
-        {...props}
-    />
-);
+const BotaoMenuMobile = (props) => {
+    const normalBorder = props.active ? 'none' : `solid 1px ${colors.branco}`;
+    const normalWeight = props.active ? 'bold' : 'normal';
+
+    return (
+        <Botao
+            css={{
+                backgroundColor: props.active ? colors.branco : colors.vermelho,
+                color: props.active ? colors.vermelho : colors.branco,
+                border: normalBorder,
+                fontWeight: normalWeight,
+                '@media (max-width: 992px)': {
+                    '&:hover': {
+                        border: normalBorder,
+                        fontWeight: normalWeight,
+                    },
+                },
+            }}
+            {...props}
+        />
+    );
+};
 
 export default BotaoMenuMobile;
