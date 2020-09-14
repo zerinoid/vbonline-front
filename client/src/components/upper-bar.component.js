a7c4e87c5e5480b53c0cd56cfb93ac760967cbc2
zerinoid <leo.zerino@gmail.com>
2020-09-14 01:19:33 -0300

    partial dedicado a upperBar

--- client/src/components/upper-bar.component.js
+++ client/src/components/upper-bar.component.js
@@ -7,21 +7,7 @@ import PlayerMenu from './player-menu.component';
 
 const UpperBar = (props) => {
     return (
-        <Row
-            css={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                margin: '0 auto',
-                width: '100%',
-                background: 'black',
-                zIndex: 1010,
-                padding: '6vh 105px',
-                '&>div': {
-                    display: 'flex',
-                },
-            }}
-        >
+        <Row className="upper-bar">
             <Col md={{ span: 4, offset: 4 }} xs={7} className="video-title">
                 <span css={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
                     {props.vimeoOptions.current_video[props.lang].title}
