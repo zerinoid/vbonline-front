ae8cc15629f3641f92f1e48a9c28b5b442b23ef9
zerinoid <leo.zerino@gmail.com>
2020-10-19 22:54:26 -0300

    ScrollHandler substituido por onEntered

--- client/src/components/lista-docs.component.js
+++ client/src/components/lista-docs.component.js
@@ -99,19 +99,9 @@ export default function ListaDocs(props) {
         texttrack: lang,
     });
 
-    const scrollHandler = () => {
-        const scrollHere = document.querySelector('#scroll-here');
-        setTimeout(function () {
-            scrollHere.scrollIntoView({ behavior: 'smooth' });
-        }, 200);
-    };
-    
     const onEntered = () => {
-        console.log('onEntered');
-    };
-    
-    const onEntering = () => {
-        console.log('onEntering');
+        const scrollHere = document.querySelector('#scroll-here');
+        scrollHere.scrollIntoView({ behavior: 'smooth' });
     };
 
     // #1 set current video
@@ -380,9 +370,8 @@ export default function ListaDocs(props) {
                             as={SetaMais}
                             variant="link"
                             eventKey="0"
-                            onClick={scrollHandler}
                         ></Accordion.Toggle>
-                        <Accordion.Collapse eventKey="0" onEntering={onEntering} onEntered={onEntered}>
+                        <Accordion.Collapse eventKey="0" onEntered={onEntered}>
                             <PreviewContainer id="scroll-here">
                                 {videos}
                             </PreviewContainer>
