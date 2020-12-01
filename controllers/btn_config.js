04b22d4e27eb1cfdb3581ca48d0be0f3386f8b0a
zerinoid <leo.zerino@gmail.com>
2020-12-01 12:56:25 -0300

    controller dos botões

---
+++ controllers/btn_config.js
@@ -0,0 +1,21 @@
+exports.getButtonConfig = (req, res, next) => {
+    data = {
+        botoes: [
+            {
+                id: 1,
+                nome: 'saiba_mais',
+                propriedades: 'something'
+            },
+            {
+                id: 2,
+                nome: 'botao_play',
+                propriedades: 'something'
+            },
+            {
+                id: 3,
+                nome: 'ficha_tecnica',
+                propriedades: 'something'
+            }
+        ]
+    }
+}
