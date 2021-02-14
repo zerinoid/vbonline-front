66d78d0d30ffe9adcf2a957dbd98b0a2dc244be3
Eduardo Haddad <eduardo.torquemada@gmail.com>
2021-02-13 22:05:09 -0300

    model Season Type

---
+++ models/SeasonType.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../db/db');
+
+const SeasonType = sequelize.define('season_type', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title_pt: {
+        type: Sequelize.STRING(1024),
+        allowNull: false,
+    },
+    title_en: {
+        type: Sequelize.STRING(1024),
+    },
+}, {
+    tableName: "season_type",
+    timestamps: false
+});
+
+module.exports = SeasonType;
